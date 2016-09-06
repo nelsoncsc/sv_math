68ff048d1e96acbc25ba8bd794cc8bafa177b64e
Nelson Campos <nelsoncscampos@gmail.com>
2016-09-06 16:56:12 -0300

    Create scr.tcl

---
+++ SQRTLOG/scr.tcl
@@ -0,0 +1,10 @@
+define_design_lib WORK -path ./design
+analyze -format sverilog SQRTLOG.sv
+elaborate SQRTLOG
+create_clock -period 500 {clock}
+compile
+write_sdc "log.sdc"
+report_timing > "time.rpt"
+report_area > "area.rpt"
+report_power > "power.rpt"
+quit
