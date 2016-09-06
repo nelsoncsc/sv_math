7777181404775bcb544bf40276ca9db6439b5221
Nelson Campos <nelsoncscampos@gmail.com>
2016-09-06 17:21:23 -0300

    Update scr.tcl

--- SQRTLOG/scr.tcl
+++ SQRTLOG/scr.tcl
@@ -3,7 +3,7 @@ analyze -format sverilog SQRTLOG.sv
 elaborate SQRTLOG
 create_clock -period 500 {clock}
 compile
-write_sdc "log.sdc"
+write_sdc "SQRTLOG.sdc"
 report_timing > "time.rpt"
 report_area > "area.rpt"
 report_power > "power.rpt"
